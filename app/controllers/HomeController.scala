6393b62b3a3c3066b73fe7fff3baeb6bcd5acb5b
steffenpedersen <steffen.pedersen@live.dk>
2018-11-17 22:52:27 +0100

    List organization repositories in terminal

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,38 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
 
+import github4s.Github
+import github4s.Github._
+import github4s.jvm.Implicits._
+import scalaj.http.HttpResponse
+
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder)
   extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
+
+    val accessToken = sys.env.get("GITHUB4S_ACCESS_TOKEN")
+
+    val listOrgRepos = Github(accessToken).repos.listOrgRepos("bleptek")
+
+    listOrgRepos.exec[cats.Id, HttpResponse[String]]() match {
+      case Left(e) => println(s"Something went wrong: ${e.getMessage}")
+      case Right(r) => println(r.result)
+    }
+
     Ok(views.html.index("Your new application is ready."))
   }
 
